14940f014371d106a29eebabe510ee61c7b62c2b
Raziuddin <raziuddin.mohd1994@gmail.com>
2026-03-19 16:30:04 +0530

    Display password min length from Identity options on Index
    Show the configured minimum password length on the Index page by injecting IOptions<IdentityOptions> into IndexModel and exposing the value to the view. Added a new paragraph in Index.cshtml to display this information.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
 
 namespace IdentityNetCore.Pages
 {
     public class IndexModel : PageModel
     {
+        public int PasswordRequiredLength { get; }
+
+        public IndexModel(IOptions<IdentityOptions> identityOptions)
+        {
+            PasswordRequiredLength = identityOptions?.Value?.Password.RequiredLength ?? 0;
+        }
         public void OnGet()
         {
 
