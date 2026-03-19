e205eb718193710d577d627220324341059154b9
Raziuddin <raziuddin.mohd1994@gmail.com>
2026-03-19 13:27:10 +0530

    Update to Identity with EF Core and SQL Server support
    Replaced Microsoft.AspNetCore.Identity with Identity.EntityFrameworkCore and added EF Core SQL Server provider. Introduced ApplicationDbContext inheriting from IdentityDbContext. Added DefaultConnection string for local SQL Server database in appsettings.json.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityNetCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
